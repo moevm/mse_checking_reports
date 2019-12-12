7b5cfed880218cb312a4ff5ac38dc8907f98e9d5
IliaAzhel <31766003+IliaAzhel@users.noreply.github.com>
2019-12-13 02:23:57 +0300

    Add files via upload
    Был изменен алгоритм сравнения: теперь при сравнении учитывается текст шаблона. Алгоритм будет проверять  на содержание в файле переданном на сравнение выделенных в шаблоне строк, абзацев, оглавлений.

--- tamplcheck/project/module/cmpFiles.py
+++ tamplcheck/project/module/cmpFiles.py
@@ -3,57 +3,56 @@
 import docx
 
 
+# анализ формата текста файла
 def analysis_doc(filename):
     """ анализ формата текста файла
-
-    :param filename: полный путь до файла + его имя
-    :return: список раздичных парметров файла"""
+        :param filename: полный путь до файла + его имя
+        :return: список раздичных парметров файла"""
     doc = docx.Document(filename)
     list_with_format = []
     dict_with_format = {}
     for paragraph in doc.paragraphs:
         # Имя стиля
-        dict_with_format['Стиль абзаца'] = paragraph.style.name
+        dict_with_format['Style of paragraph'] = paragraph.style.name
         # Горизонтальное выравнивание
-        if paragraph.paragraph_format.alignment is None:
-            dict_with_format['Горизонтальное выравнивание'] = doc.styles[
-                paragraph.style.name].paragraph_format.alignment
-        else:
-            dict_with_format['Горизонтальное выравнивание'] = paragraph.paragraph_format.alignment
+        # if paragraph.paragraph_format.alignment is None:
+        #    dict_with_format['Horizontal alignment'] = doc.styles[paragraph.style.name].paragraph_format.alignment
+        # else:
+        #    dict_with_format['Horizontal alignment'] = paragraph.paragraph_format.alignment
         # Отступ слева
         if paragraph.paragraph_format.left_indent is None:
-            dict_with_format['Отступ слева'] = doc.styles[paragraph.style.name].paragraph_format.left_indent
+            dict_with_format['Left margin'] = doc.styles[paragraph.style.name].paragraph_format.left_indent
         else:
-            dict_with_format['Отступ слева'] = paragraph.paragraph_format.left_indent
+            dict_with_format['Left margin'] = paragraph.paragraph_format.left_indent
         # Отступ первой строки абзаца
         if paragraph.paragraph_format.first_line_indent is None:
-            dict_with_format['Отступ первой строки абзаца'] = doc.styles[
-                paragraph.style.name].paragraph_format.first_line_indent
+            dict_with_format['Indent the first line of a paragraph'] = \
+                doc.styles[paragraph.style.name].paragraph_format.first_line_indent
         else:
-            dict_with_format['Отступ первой строки абзаца'] = paragraph.paragraph_format.first_line_indent
+            dict_with_format['Indent the first line of a paragraph'] = paragraph.paragraph_format.first_line_indent
         # Отступ справа
         if paragraph.paragraph_format.right_indent is None:
-            dict_with_format['Отступ справа'] = doc.styles[paragraph.style.name].paragraph_format.right_indent
+            dict_with_format['Indent on the right'] = doc.styles[paragraph.style.name].paragraph_format.right_indent
         else:
-            dict_with_format['Отступ справа'] = paragraph.paragraph_format.right_indent
+            dict_with_format['Indent on the right'] = paragraph.paragraph_format.right_indent
         # Табуляция
         # print(paragraph.paragraph_format.tab_stops)
         # Интервалы между абзацами
         if paragraph.paragraph_format.space_before is None:
-            dict_with_format['Интервалы между абзацами до'] = doc.styles[
+            dict_with_format['Intervals between paragraphs to'] = doc.styles[
                 paragraph.style.name].paragraph_format.space_before
         else:
-            dict_with_format['Интервалы между абзацами до'] = paragraph.paragraph_format.space_before
+            dict_with_format['Intervals between paragraphs to'] = paragraph.paragraph_format.space_before
         if paragraph.paragraph_format.space_after is None:
-            dict_with_format['Интервалы между абзацами после'] = doc.styles[
+            dict_with_format['Intervals between paragraphs after'] = doc.styles[
                 paragraph.style.name].paragraph_format.space_after
         else:
-            dict_with_format['Интервалы между абзацами после'] = paragraph.paragraph_format.space_after
+            dict_with_format['Intervals between paragraphs after'] = paragraph.paragraph_format.space_after
         # Межстрочный интервал
         if paragraph.paragraph_format.line_spacing is None:
-            dict_with_format['Межстрочный интервал'] = doc.styles[paragraph.style.name].paragraph_format.line_spacing
+            dict_with_format['Line spacing'] = doc.styles[paragraph.style.name].paragraph_format.line_spacing
         else:
-            dict_with_format['Межстрочный интервал'] = paragraph.paragraph_format.line_spacing
+            dict_with_format['Line spacing'] = paragraph.paragraph_format.line_spacing
         # Поведение при встрече с границей страницы
         # print(paragraph.paragraph_format.keep_together)
         # print(paragraph.paragraph_format.keep_with_next)
@@ -62,119 +61,118 @@ def analysis_doc(filename):
         for run in paragraph.runs[:1:]:
             # Шрифт
             if run.font.name is None:
-                dict_with_format['Шрифт'] = doc.styles[paragraph.style.name].font.name
+                dict_with_format['Font'] = doc.styles[paragraph.style.name].font.name
             else:
-                dict_with_format['Шрифт'] = run.font.name
+                dict_with_format['Font'] = run.font.name
             # Размера шрифта
             if run.font.size is None:
-                dict_with_format['Размера шрифта'] = doc.styles[paragraph.style.name].font.size
+                dict_with_format['Font size'] = doc.styles[paragraph.style.name].font.size
             else:
-                dict_with_format['Размера шрифта'] = run.font.size
+                dict_with_format['Font size'] = run.font.size
             # Курсив
             if run.font.italic is None:
-                dict_with_format['Курсив'] = doc.styles[paragraph.style.name].font.italic
+                dict_with_format['Italic'] = doc.styles[paragraph.style.name].font.italic
             else:
-                dict_with_format['Курсив'] = run.font.italic
+                dict_with_format['Italic'] = run.font.italic
             # Жирный
             if run.font.bold is None:
-                dict_with_format['Жирный'] = doc.styles[paragraph.style.name].font.bold
+                dict_with_format['Bold'] = doc.styles[paragraph.style.name].font.bold
             else:
-                dict_with_format['Жирный'] = run.font.bold
+                dict_with_format['Bold'] = run.font.bold
             # Подчеркнутый
             if run.font.underline is None:
-                dict_with_format['Подчеркнутый'] = doc.styles[paragraph.style.name].font.underline
+                dict_with_format['Underline'] = doc.styles[paragraph.style.name].font.underline
             else:
-                dict_with_format['Подчеркнутый'] = run.font.underline
+                dict_with_format['Underline'] = run.font.underline
+
             # Цвет
             if run.font.color.rgb is None:
-                dict_with_format['Цвет'] = doc.styles[paragraph.style.name].font.color.rgb
+                dict_with_format['Color'] = str(doc.styles[paragraph.style.name].font.color.rgb)
             else:
-                dict_with_format['Цвет'] = run.font.color.rgb
+                dict_with_format['Color'] = str(run.font.color.rgb)
+        dict_with_format['Text'] = paragraph.text
         list_with_format.append(dict_with_format)
-        # Текст абзаца
-        list_with_format.append(paragraph.text)
         dict_with_format = {}
     return list_with_format
 
 
+# Функция проверки на соответиствие шаблону
 def comparison_algorithm(template, checked):
-    """ Функция проверки на соответиствие шаблону(в разработке)
-
-    :param template: список параметров файла-шаблона
-    :param checked: список параметров исходного файла
-    :return: пустой список, если файлы сопоставимы либо список несоответствий """
-    i = 0
-    j = 0
-    mismatch_list = []
-    flag_2 = 0
-    flag = 0
-    mismatch_dict = {}
-    while i < len(template):
-        if template[i].get('Жирный') == checked[j].get('Жирный'):
-            for elem in template[i]:
-                if template[i].get(elem) != checked[j].get(elem):
-                    mismatch_dict[elem] = checked[j].get(elem)
-                    flag_2 = 1
-            if flag_2 == 1:
-                mismatch_list.append(int(j/2))
-                mismatch_list.append(mismatch_dict)
-                mismatch_dict = {}
-                flag_2 = 0
-            i += 2
-            j += 2
-        elif template[i].get('Жирный') and (checked[j].get('Жирный') is None or checked[j].get('Жирный') == False):
-            for elem in template[i]:
-                if checked[j].get(elem) != checked[j - 2].get(elem):
-                    flag = 1
+    counter_for_template = 0
+    counter_for_checked = 0
+    result = []
+    flag_in_block = 0
+    mismatch_blocks = []
+    dict_for_blocks = {}
+    while counter_for_template < len(template):
+        if template[counter_for_template].get('Text') == '':
+            counter_for_template += 1
+            continue
+        elif template[counter_for_template].get('Color') == '000000' \
+                or template[counter_for_template].get('Color') == 'None':
+            while counter_for_checked < len(checked):
+                if template[counter_for_template].get('Text') == checked[counter_for_checked].get('Text'):
+                    flag_in_block = 1
                     break
-            if flag == 0:
-                j += 2
+                counter_for_checked += 1
+            if flag_in_block == 1:
+                dict_for_blocks[counter_for_template] = counter_for_checked
             else:
-                for elem in template[i]:
-                    if template[i].get(elem) != checked[j].get(elem):
-                        mismatch_dict[elem] = checked[j].get(elem)
-                        flag_2 = 1
-                if flag_2 == 1:
-                    mismatch_list.append(int(j/2))
-                    mismatch_list.append(mismatch_dict)
-                    mismatch_dict = {}
-                    flag_2 = 0
-                i += 2
-                j += 2
-        elif (template[i].get('Жирный') is None or template[i].get('Жирный') == False) and checked[j].get('Жирный'):
-            for elem in template[i]:
-                if template[i].get(elem) != template[j - 2].get(elem):
-                    flag = 1
+                dict_for_blocks[counter_for_template] = -1
+                mismatch_blocks.append("Block:'{0}', was skipped".format(template[counter_for_template].get('Text')))
+        flag_in_block = 0
+        counter_for_template += 1
+        counter_for_checked = 0
+    if mismatch_blocks:
+        return mismatch_blocks
+    counter_for_checked = 0
+    counter_for_template = 0
+    while counter_for_template < len(template):
+        if template[counter_for_template].get('Text') == '':
+            counter_for_template += 1
+            if checked[counter_for_checked].get('Text') != '':
+                mismatch_blocks.append("An empty line has been lost in paragraph {0}".format(counter_for_checked))
+                continue
+            counter_for_checked += 1
+        elif template[counter_for_template].get('Color') == '000000' \
+                or template[counter_for_template].get('Color') == 'None':
+            if dict_for_blocks.get(counter_for_template) != -1:
+                counter_for_checked = dict_for_blocks.get(counter_for_template)
+                for key in template[counter_for_template].keys():
+                    if template[counter_for_template].get(key) != checked[counter_for_checked].get(key):
+                        result.append({counter_for_template - 1: {key: template[counter_for_template].get(key)},
+                                       counter_for_checked - 1: {key: checked[counter_for_checked].get(key)}})
+                counter_for_template += 1
+                counter_for_checked += 1
+            else:
+                counter_for_template += 1
+        else:
+            i = counter_for_template
+            while counter_for_template < len(template):
+                if dict_for_blocks.get(counter_for_template) is None:
+                    counter_for_template += 1
+                else:
+                    flag_in_block = dict_for_blocks.get(counter_for_template)
                     break
-            if flag == 0:
-                i += 2
-            else:
-                for elem in template[i]:
-                    if template[i].get(elem) != checked[j].get(elem):
-                        mismatch_dict[elem] = checked[j].get(elem)
-                        flag_2 = 1
-                if flag_2 == 1:
-                    mismatch_list.append(int(j/2))
-                    mismatch_list.append(mismatch_dict)
-                    mismatch_dict = {}
-                    flag_2 = 0
-                i += 2
-                j += 2
-        flag = 0
-    return mismatch_list
+            for j in range(counter_for_checked, flag_in_block):
+                for key in checked[j].keys():
+                    if key != 'Text' and key != 'Color':
+                        if template[i].get(key) != checked[j].get(key) and checked[j].get('Text') != '':
+                            result.append({counter_for_template - 1: {key: template[i].get(key)},
+                                           counter_for_checked - 1: {key: checked[j].get(key)}})
+    return result
 
 
 # Считывание файлов
-#template_doc = docx.Document()
-#checked_doc = docx.Document()
+# template_doc = docx.Document("template.docx")
+# checked_doc = docx.Document("checked1.docx")
 
 # словарь, в котором будем хранить информацию о формате текста документа
-#template_format = analysis_doc(template_doc)
-#checked_format = analysis_doc(checked_doc)
+# template_format = analysis_doc(template_doc)
+# checked_format = analysis_doc(checked_doc)
 # вывод полей
-#for i in range(0, len(template_format), 2):
-#    for j in template_format[i]:
-#        print(j, ":", template_format[i].get(j))
-#    print('text: ', template_format[i+1])
+# for i in range(0, len(template_format), 2):
+#   for j in template_format[i]:
+#       print(j, ":", template_format[i].get(j))
 # вывод отличий файла
-#print(comparison_algorithm(template_format, checked_format))
+# print(comparison_algorithm(template_format, checked_format))
